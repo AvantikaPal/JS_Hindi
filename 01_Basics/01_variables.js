01cda8fe355b3b46dac794dbc8b881a94016c00a
Avantika Pal <132958017+AvantikaPal@users.noreply.github.com>
2024-10-27 01:45:49 +0000

    config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "avantikapal@gmail.com"
+var accountPassword = "12345"
+  accountCity = "Delhi" 
+  let accountState;
+
+//   accountId= 2 //not allowed
+
+/*
+perfer not to use var
+bcoz of the issue in block scope and functional scope.
+
+*/
+
+console.log(accountId);
+
+console.log([accountEmail,accountId,accountPassword,accountCity, accountState]);
+
+
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all Js code as newr version
+
+
+// alert(3+3)  // we are using nodeJs, not browser
+
+// number 
+// bigint
+// String -> ""
+// boolean -> true/false
+//null  -> standard value
+// undefined ->  
+// symbol unique
+
+
+// object 
+
+
+console.log(typeof "avantika");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
