8eb06b1c9abcdccf135207acfe259a65dd82b7b4
Avantika Pal <132958017+AvantikaPal@users.noreply.github.com>
2024-10-27 09:02:42 +0000

    comparision config done

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,30 @@
+// console.log(2==2);
+// console.log(2<2);
+// console.log(2>2);
+// console.log(2>=2);
+// console.log(2<=2);
+
+
+
+// console.log("2" > 1);
+// console.log("02"> 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+/* NOTES:- 
+
+The reason is that an equality check == and comparision ><>=
+ <= work differently
+ comparision convert null to a number, treating it as 0
+ that's why (3)null >= o is true and (1)null >= 0 is false
+
+ */
+
+//    === check datatypes as well
+
+console.log("2" === 2);
+
+
